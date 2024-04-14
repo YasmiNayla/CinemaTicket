24ed10e20a3ad39112c0556557f1179849cdcbb6
Yassincase <yagar3121@oslomet.no>
2024-04-14 14:26:58 +0200

    added Js code from Oblig1. + github testing

---
+++ src/test/java/com/yassincase/CinemaTicket/CinemaTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassincase.CinemaTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
